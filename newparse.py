bef084aeda65130d4f13934d7fe42afb61a54c42
Jason Castor <castor.jason@gmail.com>
2012-12-05 11:20:01 -0800

    made changes to detect code segments as well as more types of classes, also we not get the length of the code segement (where it returns)

--- newparse.py
+++ newparse.py
@@ -4,8 +4,8 @@ import csv,re
 filepathname=sys.argv[1]
 f = open(filepathname, 'rb')
 
-#REGEX PATTERNS
-classpattern = r'(^public final class)|(^public class)' #pattern used to identify a class
+#------ REGEX PATTERNS ------#
+classpattern = r'(^public final class)|(^public class)|(^class)|(^final class)|(^private class)|(^private final class)|(^protected class)|(^protected final class) ' #pattern used to identify a class
 methodpattern = r'(^public)|(^private)|(^protected)' #pattern used to identify a method
 signaturepattern = r'(^Signature:)' #pattern used to identify a signature
 flagpattern = r'(^flags:)' #pattern used to identify a flag
@@ -13,7 +13,10 @@ gvstartpattern = r'^{' #pattern used to identify the start of global variable de
 localpattern = r'(^LocalVariableTable:)' #pattern used to identify localvariabletable
 codepattern = r'(^Code:)' #pattern used to identify Code: segment
 exceptpattern = r'(^Exceptions:)' #pattern used to identify Exceptions: segment
-#INIT REGEX
+constantpattern = r'(^Constant Value:)' #identify Constant Value
+lntpattern = r'(^LineNumberTable:)' #identify line number table
+excepttablepattern = r'(^Exception table:)' #identify exception table
+#------ INIT REGEX ------#
 classregex = re.compile(classpattern)
 methodregex = re.compile(methodpattern)
 gvstartregex = re.compile(gvstartpattern)
@@ -22,11 +25,18 @@ flagregex = re.compile(flagpattern)
 localregex = re.compile(localpattern)
 coderegex = re.compile(codepattern)
 exceptregex = re.compile(exceptpattern)
-#Variables used as flags
-gv = 0 #used to signify when GV are starting/ending
-local = 0
-firstlocal = 0 #identifies the first localvartable so that we dont print them all off twice
+constantregex = re.compile(constantpattern)
+lntregex = re.compile(lntpattern)
+excepttableregex = re.compile(excepttablepattern)
 
+#------ Variables used as flags ------#
+gv = 0 #used to signify when GV are starting/ending
+local = 0 #used to signify when localvariable table detected
+firstlocal = 0 #identifies the first localvartable so that we dont print them all off twice
+codesegment = 0
+### dict to store variable names and types
+listofvars = {}
+stackline = ""
 for line in f:
 	linestripped = line.lstrip().rstrip('\n') #removing leading whitespace and newline characters
 	findClass = classregex.match(linestripped)
@@ -35,16 +45,22 @@ for line in f:
 	findSig = sigregex.match(linestripped)
 	findLocal = localregex.match(linestripped)
 	findFlag = flagregex.match(linestripped)
+	findConstant = constantregex.match(linestripped)
+	findCode = coderegex.match(linestripped)
+	findExcept = exceptregex.match(linestripped)
+	findExceptionTable = excepttableregex.match(linestripped)
 	if findClass:
 		print "C;" + linestripped #print if class is detected
 		firstlocal = 0
 	if findMethod and "(" in linestripped  and not findClass:
 		print "M;" + linestripped #print if method detected and class not detected
-		firstlocal = 0 
+		firstlocal = 0 #reset firstlocal on a new method as it will have a new table
 	if findGV: #if the "{" was found start looking for global variables
 		gv = 1
-	if findLocal and not firstlocal:
+	if findLocal and not firstlocal: #detect the first local variable table
 		local = 1
+	if findCode or findExcept:
+		codesegment = 1
 	if findSig: #print signatures for methods/gv
 		if "length =" in linestripped:
 			pass
@@ -58,6 +74,8 @@ for line in f:
 					pass
 				elif findFlag:
 					pass #we do not want to print the flags
+				elif findConstant:
+					pass
 				else:
 					print "GV;" + linestripped #if it is not a signature or a flag it should be a global variable
 		if findMethod and "(" in linestripped: #if a method is found then that is the end of the global variables
@@ -67,7 +85,23 @@ for line in f:
 		findExcept = exceptregex.match(linestripped)
 		if not findCode and not findMethod and not findSig and not findFlag and not findExcept:
 			if linestripped != "" and "LocalVariableTable:" not in linestripped and "Start  Length" not in linestripped:
-				print linestripped
-		if findCode or findMethod or findExcept:
+				var = linestripped.split()
+				varname = var[len(var)-2]
+				vartype = var[len(var)-1]
+				listofvars[varname] = vartype
+		if findCode or findMethod or findExcept or findSig:
+			for (lvar,vvar) in listofvars.iteritems():
+				print "V; " + lvar + ";" + vvar
 			local = 0
 			firstlocal = 1
+			listofvars = {}
+	if codesegment: #if a Code: segment or and Except: segment is found (a method will have one or the other)
+		findLNT = lntregex.match(linestripped)
+
+		if "Stack=" in linestripped or "stack=" in linestripped:
+			stackline = linestripped
+		if not findLNT and not findLocal and linestripped != "}" and not findExceptionTable and not findExcept: #the code segment will end on either a local variable table or a line number table or a exception table
+			lastcodeline = linestripped
+		if findLNT or findLocal or linestripped == "}" or findExceptionTable or findExcept:
+			codesegment = 0
+			print "Code:" + stackline + ", length=" + lastcodeline.split(":")[0]
